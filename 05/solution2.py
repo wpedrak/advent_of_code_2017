fe41a0a726b39ebf95136aded6eaaaf1ea208f36
Wojciech Pedrak <woj123@gmail.com>
2020-03-27 19:17:10 +0100

    5.2 done

---
+++ 05/solution2.py
@@ -0,0 +1,16 @@
+def get_lines(filename='input'):
+    f = open(f'{filename}.txt', 'r')
+    return [line.strip() for line in f.readlines()]
+
+maze = [int(jump) for jump in get_lines()]
+
+pointer = 0
+time = 0
+
+while 0 <= pointer < len(maze):
+    time += 1
+    prev_position = pointer
+    pointer += maze[pointer]
+    maze[prev_position] += 1 if maze[prev_position] < 3 else -1
+
+print(time)
